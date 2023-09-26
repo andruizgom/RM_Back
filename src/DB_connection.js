b3879ea068198ec2acf436f12e7acd0332a382eb
Anderson <100323069+andruizgom@users.noreply.github.com>
2023-09-25 19:32:32 -0500

    Update

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 const FavoriteModel = require('./models/Favorite');
 const UserModel = require('./models/User');
 
@@ -9,12 +9,17 @@ const UserModel = require('./models/User');
 // Recuerda pasarle la información de tu archivo '.env'.
 
 // URL ----> postgres://DB_USER:DB_PASSWORD@DB_HOST/rickandmorty
+// const sequelize = new Sequelize(
+//    // URL
+//    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+//    { logging: false, native: false }
+// );
+
 const sequelize = new Sequelize(
    // URL
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+   DB_DEPLOY,
    { logging: false, native: false }
 );
-
 // EJERCICIO 05
 // Debajo de este comentario puedes ejecutar la función de los modelos.
 
